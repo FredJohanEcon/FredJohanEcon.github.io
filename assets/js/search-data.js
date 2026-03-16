c8690e68d91c302e1a7a35d9247365a21611e271
FredJohanEcon <126968796+FredJohanEcon@users.noreply.github.com>
2026-03-16 13:41:06 +0000

    Deploying to gh-pages from @ FredJohanEcon/FredJohanEcon.github.io@ef7090d8261a5b596a7bafcbd0feaa8148f75a2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,12 +413,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+      },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
